ebafa21574cb8b8848bae3ddb43ac75b7a86eb7a
Stanislav Ryabov <Stasonline26@gmail.com>
2022-10-19 21:52:02 +0300

    add Example2

---
+++ HomeworkCsharp/Homework2/Example2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.Write("введите любое целое число: ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if(number > 99)
+{
+    Console.WriteLine(number.ToString()[2]);
+}
+else
+{
+    Console.WriteLine("в данном числе нет третьей цифры");
+}
