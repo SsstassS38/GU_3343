7b0d0ab07203c90e0d11c70e4b0a24e753ef5e6d
Stanislav Ryabov <Stasonline26@gmail.com>
2022-11-13 12:09:16 +0300

    add new Examples of 5,6 seminars and 5 lection

---
+++ origin_main/Examples/Example14/Program.cs
@@ -0,0 +1,26 @@
+﻿// рекурсия - факториал
+
+/*
+int Factorial (int n)
+{
+    // 1!=1
+    // 0!=1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+{
+    Console.WriteLine(Factorial(5));
+}
+*/
+
+double Factorial (int n)
+{
+    // 1!=1
+    // 0!=1
+    if(n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
