4ac81710666f66825d2d8bd9e6ae6a14f83e4e13
Stanislav Ryabov <Stasonline26@gmail.com>
2022-11-16 11:19:50 +0300

    add Example38

---
+++ HomeworkCsharp/Homework5/Example38/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+int[] InitArray()
+{
+    int[] result = new int[4];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(1, 100);
+    }
+    return result;
+}
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int GetNumberMax(int[] array)
+{
+    int numbermax = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > numbermax)
+            numbermax = array[i];
+    }
+    return numbermax;
+}
+int GetNumberMin(int[] array)
+{
+    int numbermin = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < array[0])
+            numbermin = array[i];
+    }
+    return numbermin;
+}
+int[] array = InitArray();
+Print(array);
+int result = GetNumberMax(array) - GetNumberMin(array);
+Console.WriteLine($"Разница между максимальным эл-ом и минимальным эл-м: {result} ");
---
+++ HomeworkCsharp/Homework6/Example41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int GetPositive(int[] array)
+{
+    int positiveSumm = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0)
+            positiveSumm+= array[i];
+    }  
+    return positiveSumm;
+}
+int[] array =;
+PrintArray(array);
+int positiveSumm = GetPositive(array);
+Console.WriteLine($"Сумма положительных чисел: {positiveSumm},");
+
