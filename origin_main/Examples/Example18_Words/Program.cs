26ae475f6eb5c66fdd4287edcbbce67d930defcb
Stanislav Ryabov <Stasonline26@gmail.com>
2022-11-20 15:02:30 +0300

    add ExamplesOf7Lection

---
+++ origin_main/Examples/Example18_Words/Program.cs
@@ -0,0 +1,22 @@
+﻿// Перербор слов 
+
+
+char[] s = { 'а', 'и', 'с', 'в' };
+int count = s.Length;
+int n = 1;
+for (int i = 0; i < count; i++)
+{
+    for (int j = 0; j < count; j++)
+    {
+        for (int k = 0; k < count; k++)
+        {
+            for (int l = 0; l < count; l++)
+            {
+                for (int m = 0; m < count; m++)
+                {
+                    Console.WriteLine($"{n++,-5}{s[i]}{s[j]}{s[k]}{s[l]}{s[m]}");
+                }
+            }
+        }
+    }
+}
---
+++ origin_main/Examples/Example20_OpenFolder/Program.cs
@@ -0,0 +1,17 @@
+﻿//Посмотреть содержимое папки
+
+void CatalogInfo(string path, string indent = "")
+{
+ DirectoryInfo catalogs = new DirectoryInfo(path);
+ foreach (var currentCatalog in catalogs.GetDirectories())
+ {
+ Console.WriteLine($"{indent}{currentCatalog.Name}");
+ CatalogInfo(currentCatalog.FullName, indent + " ");
+ }
+ foreach (var item in catalogs.GetFiles())
+ {
+ Console.WriteLine($"{indent}{item.Name}");
+ }
+}
+string path = @"D:\Hello Git\GB 3343\GU_3343";
+CatalogInfo(path);
